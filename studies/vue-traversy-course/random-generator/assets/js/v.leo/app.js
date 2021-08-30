a1e3ad87735f28ef1a820ba992b6ba3e5fd80041
anotherleo <lyrafurg@gmail.com>
2021-08-30 15:08:27 -0300

    Update app.js

--- studies/vue-traversy-course/random-generator/assets/js/v.leo/app.js
+++ studies/vue-traversy-course/random-generator/assets/js/v.leo/app.js
@@ -16,7 +16,7 @@ const APP = Vue.createApp({
 			const { results } = await RES.json();
 					
 			console.log(results);
-				
+	
 			this.firstName = results[0].name.first;
 			this.lastName = results[0].name.last;
 			this.email = results[0].email;
