bb0796f9df4d406eaae9a780c0222baf8146eb1b
anotherleo <lyrafurg@gmail.com>
2021-12-27 18:09:53 -0300

    leo's web dev -- tabs

--- js-toybox/leo-seniority/assets/js/skill-measure.js
+++ js-toybox/leo-seniority/assets/js/skill-measure.js
@@ -1,14 +1,21 @@
+/* variables */
 const HTML_METER = document.querySelector('#js-skill-html-meter');
 const HTML_NUMBER = document.querySelector('#js-skill-html-number');
 const HTML_SKILL = document.querySelector('#js-skill-html-checklist');
-
 let htmlSkill = HTML_SKILL.getElementsByTagName("li").length;
 let htmlChecks = HTML_SKILL.querySelectorAll('input[type="checkbox"]:checked').length;
 
+/* general operations */
+function percent(a, b) {
+    return ((a/b) * 100).toFixed(1) + '%';
+}
+
+/* individual calcs */
 function htmlSkillCounter() {
     HTML_METER.max = htmlSkill;
     HTML_METER.value = htmlChecks;
-    HTML_NUMBER.innerHTML=`${htmlChecks}/${htmlSkill}`;
+    HTML_NUMBER.innerHTML=`${percent(htmlChecks, htmlSkill)}`;
 }
 
+/* run */
 htmlSkillCounter();
