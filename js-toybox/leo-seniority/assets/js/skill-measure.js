cd0faff70eae4b50f0c910e0596e743fb34d645d
anotherleo <lyrafurg@gmail.com>
2021-12-27 22:26:20 -0300

    leo's web dev -- higher tier sample

--- js-toybox/leo-seniority/assets/js/skill-measure.js
+++ js-toybox/leo-seniority/assets/js/skill-measure.js
@@ -1,9 +1,17 @@
 /* variables */
+const HTML_SKILL = document.querySelector('#js-skill-html-checklist');
+
 const HTML_METER = document.querySelector('#js-skill-html-meter');
 const HTML_NUMBER = document.querySelector('#js-skill-html-number');
-const HTML_SKILL = document.querySelector('#js-skill-html-checklist');
 let htmlSkill = HTML_SKILL.getElementsByTagName("li").length;
 let htmlChecks = HTML_SKILL.querySelectorAll('input[type="checkbox"]:checked').length;
+
+const HTML_METER_HIGH = document.querySelector('#js-skill-html-meter-high');
+const HTML_NUMBER_HIGH = document.querySelector('#js-skill-html-number-high');
+let htmlSkillHigh = HTML_SKILL.getElementsByClassName("js-high").length;
+let htmlChecksHigh = HTML_SKILL.querySelectorAll('input[type="checkbox"].advance:checked').length;
+
+console.log(htmlSkill, htmlChecks, htmlSkillHigh, htmlChecksHigh)
 
 /* general operations */
 function percent(a, b) {
@@ -15,7 +23,12 @@ function htmlSkillCounter() {
     HTML_METER.max = htmlSkill;
     HTML_METER.value = htmlChecks;
     HTML_NUMBER.innerHTML=`${percent(htmlChecks, htmlSkill)}`;
+
+    HTML_METER_HIGH.max = htmlSkillHigh;
+    HTML_METER_HIGH.value = htmlChecksHigh;
+    HTML_NUMBER_HIGH.innerHTML=`${percent(htmlChecksHigh, htmlSkillHigh)}`;
 }
+
 
 /* run */
 htmlSkillCounter();
