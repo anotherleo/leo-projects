a6911adc2dbbc2ab4b44f16f31dcb19868fc3675
anotherleo <lyrafurg@gmail.com>
2021-12-30 08:19:00 -0300

    still with a bug in the higher counter

--- js-toybox/leo-seniority/assets/js/skill-measure.js
+++ js-toybox/leo-seniority/assets/js/skill-measure.js
@@ -1,4 +1,9 @@
-/* variables */
+/* general operations */
+function percent(a, b) {
+    return ((a/b) * 100).toFixed(1) + '%';
+}
+
+/* HTML */
 const HTML_SKILL = document.querySelector('#js-skill-html-checklist');
 
 const HTML_METER = document.querySelector('#js-skill-html-meter');
@@ -9,16 +14,8 @@ let htmlChecks = HTML_SKILL.querySelectorAll('input[type="checkbox"]:checked').l
 const HTML_METER_HIGH = document.querySelector('#js-skill-html-meter-high');
 const HTML_NUMBER_HIGH = document.querySelector('#js-skill-html-number-high');
 let htmlSkillHigh = HTML_SKILL.getElementsByClassName("js-high").length;
-let htmlChecksHigh = HTML_SKILL.querySelectorAll('input[type="checkbox"].advance:checked').length;
+let htmlChecksHigh = HTML_SKILL.getElementsByClassName('js-advance:checked').length;
 
-console.log(htmlSkill, htmlChecks, htmlSkillHigh, htmlChecksHigh)
-
-/* general operations */
-function percent(a, b) {
-    return ((a/b) * 100).toFixed(1) + '%';
-}
-
-/* individual calcs */
 function htmlSkillCounter() {
     HTML_METER.max = htmlSkill;
     HTML_METER.value = htmlChecks;
@@ -29,6 +26,29 @@ function htmlSkillCounter() {
     HTML_NUMBER_HIGH.innerHTML=`${percent(htmlChecksHigh, htmlSkillHigh)}`;
 }
 
+/* variables CSS */
+const CSS_SKILL = document.querySelector('#js-skill-css-checklist');
+
+const CSS_METER = document.querySelector('#js-skill-css-meter');
+const CSS_NUMBER = document.querySelector('#js-skill-css-number');
+let cssSkill = CSS_SKILL.getElementsByTagName("li").length;
+let cssChecks = CSS_SKILL.querySelectorAll('input[type="checkbox"]:checked').length;
+
+const CSS_METER_HIGH = document.querySelector('#js-skill-css-meter-high');
+const CSS_NUMBER_HIGH = document.querySelector('#js-skill-css-number-high');
+let cssSkillHigh = CSS_SKILL.getElementsByClassName('js-high').length;
+let cssChecksHigh = CSS_SKILL.querySelectorAll('input[type="checkbox"].js-advance:checked').length;
+
+function cssSkillCounter() {
+    CSS_METER.max = cssSkill;
+    CSS_METER.value = cssChecks;
+    CSS_NUMBER.innerHTML=`${percent(cssChecks, cssSkill)}`;
+
+    CSS_METER_HIGH.max = cssSkillHigh;
+    CSS_METER_HIGH.value = cssChecksHigh;
+    CSS_NUMBER_HIGH.innerHTML=`${percent(cssChecksHigh, cssSkillHigh)}`;
+}
 
 /* run */
 htmlSkillCounter();
+cssSkillCounter();
