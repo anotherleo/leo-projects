664bee39b2364137bf3d81feb4cbe58b454798e6
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2022-11-29 19:46:31 -0300

    projeto

--- studies-v5/dnd-gallery/src/pages/HomePage/index.jsx
+++ studies-v5/dnd-gallery/src/pages/HomePage/index.jsx
@@ -1,7 +1,16 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
 
 export default function HomePage() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <Menu />
+                <section className="page__banner">
+                    
+                </section>
+            </main>
+        </>
     )
 }
