788b90e40b52b7937c31b11bc921d2fd3b750c6a
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2022-12-13 16:49:57 -0300

    finalização

--- studies-v5/alura-space/src/components/Gallery/index.jsx
+++ studies-v5/alura-space/src/components/Gallery/index.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import Cards from './Cards'
 import styles from './Gallery.module.scss'
 import photos from './photos.json'
 
 export default function Gallery() {
+  const [itens, setItens] = useState(photos)
+  const tags = [...new Set(photos.map((value) => value.tag))]
+
+  const filterPhotos = (tag) => {
+    const newPhotos = photos.filter((photo) => {
+      return photo.tag === tag;
+    })
+
+    setItens(newPhotos);
+  }
+
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards photos={photos} styles={styles} />
+      <Tags tags={tags} filterPhotos={filterPhotos} setItens={setItens} />
+      <Cards photos={itens} styles={styles} />
     </section>
   )
 }
