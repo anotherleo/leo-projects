c49ef77b2490ff9bec8f688ccc646ce6ac466855
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2022-12-18 17:20:14 -0300

    teste de fonte

--- studies-v5/exercise-filter-and-theme/src/pages/_app.js
+++ studies-v5/exercise-filter-and-theme/src/pages/_app.js
@@ -1,16 +1,5 @@
 import '../styles/globals.scss'
 
-import { Nunito } from '@next/font/google'
-
-const nunito = Nunito({
-  weight: '400',
-  subsets: ['latin'],
-})
-
 export default function App({ Component, pageProps }) {
-  return (
-    <div className={nunito.className}>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
